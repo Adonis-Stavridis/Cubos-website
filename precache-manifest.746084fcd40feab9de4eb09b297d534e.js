94518098068004508df8880baab2b125b94310a0
Adonis Stavridis <adonis.stavridis@gmail.com>
2020-05-28 02:35:28 +0200

    Updates

---
+++ precache-manifest.746084fcd40feab9de4eb09b297d534e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d845881e4293d3847d355853ec26720",
+    "url": "/Cubos-website/index.html"
+  },
+  {
+    "revision": "bbdd3d374951d55f7f19",
+    "url": "/Cubos-website/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "62534abfc0eed35bf97b",
+    "url": "/Cubos-website/static/css/main.a0f8a06f.chunk.css"
+  },
+  {
+    "revision": "bbdd3d374951d55f7f19",
+    "url": "/Cubos-website/static/js/2.19b765cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Cubos-website/static/js/2.19b765cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62534abfc0eed35bf97b",
+    "url": "/Cubos-website/static/js/main.d3d4e3f7.chunk.js"
+  },
+  {
+    "revision": "3dae1d12f918a78e239a",
+    "url": "/Cubos-website/static/js/runtime-main.8b117561.js"
+  },
+  {
+    "revision": "5cb16bb160237b346201852e6ac6c140",
+    "url": "/Cubos-website/static/media/cubos.5cb16bb1.png"
+  }
+]);
