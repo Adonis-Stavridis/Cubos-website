3b54383b65e51a043d8c88f24ae65491d4a8c1f2
Adonis-Stavridis <adonis.stavridis@gmail.com>
2020-06-07 04:14:56 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Cubos-website/precache-manifest.e22d5156565dcf4d1fa1cf3a68fa53c6.js"
+  "/Cubos-website/precache-manifest.8b8a209532de493d4bcdbccf34319f0b.js"
 );
 
 self.addEventListener('message', (event) => {
